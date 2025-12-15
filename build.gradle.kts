92414234ec58a90f304c89c708693d4d8e54f9a8
bfalls <bfalls@outlook.com>
2025-12-14 21:14:30 -0700

    Add CivTAK SDK and upgrade Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // Define plugins for submodules
     id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
